ea304a6d08688cf5d8a03c46f54bd0f94dc2f292
LuoQingy <1075899733@qq.com>
2019-08-08 16:06:51 +0800

    添加admin

---
+++ react-admin/build/precache-manifest.e90b6878dc2bf4bcca056020daed5d70.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b95d27f7b4ef9c9ee1c",
+    "url": "./static/js/3.0b95d27f.chunk.js"
+  },
+  {
+    "revision": "1c9fafb03f3fd8d6d5b4",
+    "url": "./static/css/main.0baa3fa3.chunk.css"
+  },
+  {
+    "revision": "5755c7a4583d4c7f046e",
+    "url": "./static/js/1.5755c7a4.chunk.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "819f3c70fbe1fcb16498",
+    "url": "./static/js/2.819f3c70.chunk.js"
+  },
+  {
+    "revision": "3c9ecb331155d54c5a1a5f0b35be36a9",
+    "url": "./static/media/y6oxFxU60dYw9khW6q8jGw.3c9ecb33.woff2"
+  },
+  {
+    "revision": "1c9fafb03f3fd8d6d5b4",
+    "url": "./static/js/main.1c9fafb0.chunk.js"
+  },
+  {
+    "revision": "3ec552c4cf43006f65ef",
+    "url": "./static/js/runtime~main.3ec552c4.js"
+  },
+  {
+    "revision": "553c69e9bcf06d9f6218730fc13fcdda",
+    "url": "./static/media/b1.553c69e9.jpg"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "0b95d27f7b4ef9c9ee1c",
+    "url": "./static/css/3.c32c35d5.chunk.css"
+  },
+  {
+    "revision": "819f3c70fbe1fcb16498",
+    "url": "./static/css/2.ff7aa488.chunk.css"
+  },
+  {
+    "revision": "bac47fcff1568f9730c7885e0498f957",
+    "url": "./index.html"
+  }
+];
