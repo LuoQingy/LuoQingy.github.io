01dbd514569bba5f2813fdc44660027fb1b84800
LuoQingy <1075899733@qq.com>
2019-08-08 12:01:46 +0800

    更新reactad

---
+++ reactda/build/controlCenter.js
@@ -0,0 +1,9 @@
+var controlCanvas = function canvass(){
+    return true;//用Boolean来控制画布的上下线
+}
+var controlList = function lists(){
+    return true;//用Boolean来控制列表广告的上下线
+}
+var controlDetail = function details(){
+    return true;//用Boolean来控制详情内容广告的上下线
+}
---
+++ reactda/build/precache-manifest.c3db93112c49d32c3ce86db348a85d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01749b7b4dc53ac666d138502e1afcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742ea126ed9a0e2f0183",
+    "url": "/static/css/main.8a5f6100.chunk.css"
+  },
+  {
+    "revision": "fd9e8d77a75db3e41c2b",
+    "url": "/static/js/2.b452891e.chunk.js"
+  },
+  {
+    "revision": "742ea126ed9a0e2f0183",
+    "url": "/static/js/main.b389d255.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
